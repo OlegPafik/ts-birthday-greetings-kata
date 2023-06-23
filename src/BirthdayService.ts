b0641cf33ed3c2be8a622525702ae9e54e550119
Oleg <oleg.pafik@eversens.com>
2023-06-23 18:25:14 +0200

    refactor(DbService): Change from getAll lines to getAllEmployees, returning an array of Employee

--- src/BirthdayService.ts
+++ src/BirthdayService.ts
@@ -1,4 +1,4 @@
-import { Employee } from './Employee'
+
 import { OurDate } from './OurDate'
 import { DbService } from './DbService'
 import { MailService } from './MailService'
@@ -7,12 +7,10 @@ export class BirthdayService {
     sendGreetings(fileName: string, ourDate: OurDate, smtpHost: string, smtpPort: number) {
         const dbService = new DbService(fileName)
         const mailService = new MailService(smtpHost, smtpPort, 'sender@here.com')
-        const employees = dbService.getAll();
+        const employees = dbService.getAllEmployees();
 
         // print all lines
-        employees.forEach((line) => {
-            const employeeData = line.split(', ')
-            const employee = new Employee(employeeData[1], employeeData[0], employeeData[2], employeeData[3])
+        employees.forEach((employee) => {
             if (employee.isBirthday(ourDate)) {
                 const recipient = employee.getEmail()
                 const body = 'Happy Birthday, dear %NAME%!'.replace('%NAME%',
--- src/DbService.ts
+++ src/DbService.ts
@@ -1,5 +1,6 @@
 import fs from 'fs'
 import path from 'path'
+import { Employee } from './Employee'
 
 export class DbService {
     private readonly _fileName: string
@@ -8,14 +9,18 @@ export class DbService {
         this._fileName = fileName
     }
 
-
-    getAll(): string[] {
+    getAllEmployees(): Employee[] {
         const data = fs.readFileSync(path.resolve(__dirname, `../resources/${this._fileName}`), 'UTF-8')
 
         // split the contents by new line
         const lines = data.split(/\r?\n/)
         lines.shift()
 
-        return lines
+        const employees = lines.map((line) => {
+            const employeeData = line.split(', ')
+            return new Employee(employeeData[1], employeeData[0], employeeData[2], employeeData[3])
+        })
+
+        return employees
     }
 }
