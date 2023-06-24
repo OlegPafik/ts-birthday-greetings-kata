6601ef18f91975748d857bcfe922ae8ccce4fb05
Oleg <oleg.pafik@eversens.com>
2023-06-24 12:50:27 +0200

    refactor(BirthDayService.test): Rename mailManager and birthDayService

--- test/BirthdayService.test.ts
+++ test/BirthdayService.test.ts
@@ -4,23 +4,23 @@ import { BirthdayService } from '../src/BirthdayService'
 import { MockMailManager } from './_fixtures/MockMailManager'
 
 describe('BirthdayService', () => {
-    let dbService: MockEmployeesRepository
-    let mailService: MockMailManager
+    let employeesRepository: MockEmployeesRepository
+    let mailManager: MockMailManager
     let birthDayService: BirthdayService
 
     beforeEach(() => {
-        dbService = new MockEmployeesRepository()
-        mailService = new MockMailManager()
-        birthDayService = new BirthdayService(dbService, mailService)
+        employeesRepository = new MockEmployeesRepository()
+        mailManager = new MockMailManager()
+        birthDayService = new BirthdayService(employeesRepository, mailManager)
     })
 
     it('base scenario', async () => {
         birthDayService.sendGreetings(new OurDate('2008/10/08'))
-        expect(mailService.timesCalled).toEqual(1)
+        expect(mailManager.timesCalled).toEqual(1)
     })
 
     it('will not send emails when nobodys birthday', async () => {
         birthDayService.sendGreetings(new OurDate('2008/01/01'))
-        expect(mailService.timesCalled).toEqual(1)
+        expect(mailManager.timesCalled).toEqual(1)
     })
 })
